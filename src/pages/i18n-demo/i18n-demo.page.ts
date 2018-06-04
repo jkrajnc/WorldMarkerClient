a0c9d96b8ecd636faf5d8d0a6e8f14903fa44647
dansmon <dani.smon@gmail.com>
2018-06-04 23:35:53 +0200

    enter

--- src/pages/i18n-demo/i18n-demo.page.ts
+++ src/pages/i18n-demo/i18n-demo.page.ts
@@ -5,6 +5,8 @@ import { NavController } from 'ionic-angular';
 import { availableLanguages, sysOptions } from './i18n-demo.constants';
 import { TranslateService } from 'ng2-translate';
 import { HomePage } from '../home/home';
+import { MapPage } from '../map/map';
+import { ViewActivityPage } from '../viewActivity/viewActivity';
 
 @Component({
 	templateUrl: 'i18n-demo.html'
@@ -25,8 +27,20 @@ export class I18nDemoPage {
 	applyLanguage() {
 		this.translate.use(this.selectedLanguage);
 	}
-    goAnOtherPage(){
+ 
+
+
+	maptab(){
+		this.navCtrl.push(MapPage);
+  	}
+  
+  hometab(){
 		this.navCtrl.push(HomePage);
-	}
+  	}
+  
+  viewActivitytab(){
+		this.navCtrl.push(ViewActivityPage);
+  }
+
 
 }
