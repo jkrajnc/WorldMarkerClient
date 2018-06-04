4845de495fd85d735b28a5542c430ea72227da80
dansmon <dani.smon@gmail.com>
2018-06-04 23:32:37 +0200

    enter

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { I18nDemoPage } from '../i18n-demo/i18n-demo.page';
 import { MapPage } from '../map/map';
+import { ViewActivityPage } from '../viewActivity/viewActivity';
 
 @Component({
   selector: 'page-home',
@@ -13,9 +14,7 @@ export class HomePage {
 
   }
 
-  goAnOtherPage(){
-		this.navCtrl.push(I18nDemoPage);
-  }
+
   
   maptab(){
 		this.navCtrl.push(MapPage);
@@ -23,7 +22,12 @@ export class HomePage {
   
   settingstab(){
 		this.navCtrl.push(I18nDemoPage);
-	}
+  }
+  
+  viewActivitytab(){
+		this.navCtrl.push(ViewActivityPage);
+  }
+
 
 
 }
